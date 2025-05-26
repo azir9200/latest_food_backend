a1d00958bc45d3718ed6c8a10bd6d7f5a98c95f2
Azir@9200 <aziruddin83@hotmail.com>
2025-05-26 21:30:32 +0100

    app router and app folder created

---
+++ src/app/router/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+
+const router = Router();
+const AllRoutes = [
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+ 
+];
+
+AllRoutes.forEach((item) => router.use(item.path, item.route));
+export default router;
